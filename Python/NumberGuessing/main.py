91cce8532f1500f12e70c865f8d59fa5dd4fe4ac
KIKO <102423868+FranciscoAraujo2@users.noreply.github.com>
2023-02-02 16:34:31 +0000

    Update main.py

--- Python/NumberGuessing/main.py
+++ Python/NumberGuessing/main.py
@@ -1,9 +1,14 @@
 import random
 
 def main():
+    # print prompt to user
     print("Guess the number between 1 and 100")
+    
+    # generate a random number between 1 and 100
     number = random.randint(1,100)
     guess = None
+    
+    # loop until user correctly guesses the number
     while guess != number:
         guess = int(input("Enter your guess: "))
         if guess < number:
@@ -12,5 +17,6 @@ def main():
             print("Too high, try again.")
     print(f"You got it!. \nNumber: {number}")
 
+# run main function if the script is run directly
 if __name__ == '__main__':
     main()
